97efc8132b3caa18e16da6ff3bb51104099f7d00
dvar98 <varela13guerrero@gmail.com>
2024-08-09 18:18:30 -0500

    Update dfa_dao.c

--- dfa_dao.c
+++ dfa_dao.c
@@ -6,13 +6,32 @@
 DFA* dfa_crear(const char* nombre_archivo){
 
     FILE *pf;
-    pf =  fopen("datos", "r+");
+    pf =  fopen( nombre_archivo, "r");
     if (pf == NULL)
     {
         printf("Error el archivo no existe");
-        exit(EXIT_FAILURE);
+        return NULL;
     }
     
+
+    //Obtener los estados
+    char estado[2000];
+    fgets(estado,2000,nombre_archivo);
+    DFA* dfa = malloc(sizeof(DFA));
+    dfa -> estados = malloc(sizeof(char*) * 20);
+    dfa -> num_estados = 0;
+    char* separador = strtok(estado, ",");
+    while (separador)
+    {
+        dfa -> estados[dfa -> num_estados] = malloc(strlen(separador) + 1);
+        strcpy(dfa -> estados[dfa -> num_estados], separador);
+        dfa -> num_estados++;
+        token
+
+    }
+    
+
+
     fclose(pf);
-    return EXIT_SUCCESS;
+    return dfa;
 }
